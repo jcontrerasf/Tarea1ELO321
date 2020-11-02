e24e2a2f2173a8af988f352e345ebd35dd646fb8
jcontrerasf <50180934+jcontrerasf@users.noreply.github.com>
2020-11-01 21:06:32 -0300

    2do commit
    reemplacé tabs por espacios, añadí funciones y agregué readme.txt

---
+++ README.txt
@@ -0,0 +1,15 @@
+Integrante 1:
+Julio Erik Contreras Fuica
+ROL: 201721059-1
+e-mail: julio.contrerasf@sansano.usm.cl
+
+Integrante 2:
+Cris
+ROL:2017
+e-mail: 
+
+
+- parteA.c Corresponde al código para la parte A de la tarea.
+Compilar como: $ gcc parteA.c -o parteA
+Ejectutar como: $ ./parteA n
+Donde n corresponde al número para comenzar la Conjetura de Collatz
--- parteA.c
+++ parteA.c
@@ -1,30 +1,74 @@
 //! ELO321 - Teoría de Sistemas Operativos, 2020-2
 /*!
 * @file   : parteA.c 
-* @author   : Julio Contreras Fuica
+* @author : Julio Contreras Fuica
+* @author : Cristian González Bustos
 * @date   : 01/11/2020
 * @brief  : Parte A de la Tarea 1 
 */
 
+//RECORDAR PASAR TABS A ESPACIOS
+//RECORDAR PASAR TABS A ESPACIOS
+//RECORDAR PASAR TABS A ESPACIOS
+//RECORDAR PASAR TABS A ESPACIOS
+//RECORDAR PASAR TABS A ESPACIOS
+//RECORDAR PASAR TABS A ESPACIOS
+//RECORDAR PASAR TABS A ESPACIOS
+
 #include <time.h>
 #include <stdio.h>
-#include <string.h> 
+#include <stdlib.h>
+#include <string.h>
+
+/*! \fn sucesion_Collatz (unsigned int n)
+    \brief Implimenta la susesión de Collatz
+    \param n Número a operar
+*/
+int sucesion_Collatz (unsigned int n);
 
 
 int main (int argc, char *argv[]){
-	int n;
-	if (argc != 2) //Verifica si hay solo un argumento
-	{
-		printf("Debes ingresar un solo numero\n");
-	} else {
-		n = atoi(argv[1]);
-		if (n <= 0) //El argumento es un string o menor a 1
-		{
-			printf("El numero %d no es valido\n", n);
-			return -1;
-		} else {
-			printf("El numero %d es valido\n", n);
-			return 0;
-		}
-	}
+    int n;
+    pid_t pid1;
+    time_t tiempo;
+
+    if (argc != 2) //Verifica si hay solo un argumento
+    {
+        printf("Debes ingresar un solo argumento\n");
+        return -1;
+    } else {
+        n = atoi(argv[1]);
+        if (n <= 0) //El argumento es un string o menor a 1
+        {
+            printf("El número %d no es válido\n", n);
+            return -1;
+        } else {
+            //printf("El número %d sí es válido\n", n);
+            pid1 = fork();
+            if (pid1 < 0)
+            {
+                printf("Error al intentar crear el proceso hijo");
+            }
+            else if (pid1 == 0){ //Proceso hijo
+                time(&tiempo);
+                printf("Hora de ejecución: %s", ctime(&tiempo));
+                exit(1); //Se termina el proceso hijo
+                //NO SE SI VA 0 O 1
+                //NO SE SI VA 0 O 1
+                //NO SE SI VA 0 O 1
+            } else {
+                wait(); //Se espera a que termine el proceso hijo
+            }
+            return 0;
+        }
+    }
+}
+
+int sucesion_Collatz (unsigned int n){
+    if (n%2 == 0) //Es par
+    {
+        return n/2;
+    } else {
+        return 3*n+1;
+    }
 }
