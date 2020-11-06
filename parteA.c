7b8dbe7d6c873220c5c32b458c196d1b803fa18c
jcontrerasf <50180934+jcontrerasf@users.noreply.github.com>
2020-11-06 18:38:30 -0300

    Update parteA.c
    Se quitaron else innecesarios. Funcion Collatz con proceso hijo funciona.

--- parteA.c
+++ parteA.c
@@ -32,43 +32,67 @@ int main (int argc, char *argv[]){
     pid_t pid1;
     time_t tiempo;
 
+    const int SIZE = 4096;
+    const char *name = "/shm";
+    int shm_fd;
+    void *ptr;
+
     if (argc != 2) //Verifica si hay solo un argumento
     {
         printf("Debes ingresar un solo argumento\n");
         return -1;
-    } else {
-        n = atoi(argv[1]);
-        if (n <= 0) //El argumento es un string o menor a 1
-        {
-            printf("El número %d no es válido\n", n);
-            return -1;
-        } else {
-            //printf("El número %d sí es válido\n", n);
-            pid1 = fork();
-            if (pid1 < 0)
-            {
-                printf("Error al intentar crear el proceso hijo");
-            }
-            else if (pid1 == 0){ //Proceso hijo
-                time(&tiempo);
-                printf("Hora de ejecución: %s", ctime(&tiempo));
-                exit(1); //Se termina el proceso hijo
-                //NO SE SI VA 0 O 1
-                //NO SE SI VA 0 O 1
-                //NO SE SI VA 0 O 1
-            } else {
-                wait(); //Se espera a que termine el proceso hijo
-            }
-            return 0;
-        }
     }
+    n = atoi(argv[1]); //El argumento se convierte a entero
+    if (n <= 0) //El argumento es un string o menor a 1
+    {
+        printf("El número %d no es válido\n", n);
+        return -1;
+    }
+    //    } else { está bien haberlo quitado?????????????????????????????????????????????
+    //printf("El número %d sí es válido\n", n);
+    pid1 = fork();
+    if (pid1 < 0)
+        printf("Error al intentar crear el proceso hijo");
+    else if (pid1 == 0){ //Proceso hijo
+        time(&tiempo);
+        printf("Hora de ejecución: %s", ctime(&tiempo));
+        exit(0); //Se termina el proceso hijo
+    } else
+        wait(); //Se espera a que termine el proceso hijo
+/*
+    //Se crea el segmento de memoria compartida
+    shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+
+    //Se configura el tamaño de la memoria compartida
+    ftruncate(shm_fd,SIZE);
+
+    //now map the shared memory segment in the address space of the process
+    ptr = mmap(0,SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (ptr == MAP_FAILED) {
+        printf("Map failed\n");
+        return -1;
+    }
+*/
+    pid1 = fork(); //Se reutiliza la variable pid1
+    if (pid1 < 0)
+        printf("Error al intentar crear el proceso hijo");
+    else if (pid1 == 0){ //Proceso hijo
+        printf("numero =%d\n", n);
+        while(n > 1){
+            n = sucesion_Collatz(n);
+            printf("numero =%d\n", n);
+        }
+        exit(0); //Se termina el proceso hijo
+    } else
+        wait(); //Se espera a que termine el proceso hijo
+    return 0;
 }
 
 int sucesion_Collatz (unsigned int n){
     if (n%2 == 0) //Es par
     {
         return n/2;
-    } else {
+    } else { //Es impar
         return 3*n+1;
     }
 }
